26abc1ad416891febeaf69a49318e14a86b89470
Stefan Collier <sc22g13@soton.ac.uk>
2016-11-16 13:07:08 +0000

    Prototype of 32 image processor

--- service/olivia/vectorizer.py
+++ service/olivia/vectorizer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os.path
+import os
 
 from neon import NervanaObject
 from neon.util.persist import load_obj
@@ -21,6 +22,7 @@ class Vectorizer:
     layer = None
     patch_width = None
     patch_height = None
+    backend = None
 
     def __init__(self, prm_path=default_prm_path, layer=-4, backend='gpu'):
         print 'Log::Vectorizer:: Initialising Vectorizer'
@@ -34,7 +36,7 @@ class Vectorizer:
         if backend == 'cpu':
             cores = 1
         gen_backend(batch_size=cores, backend=backend)
-
+        self.backend = backend
 
         print 'Log::Vectorizer:: Loading model from %s' % prm_path
         model_dict = load_obj(prm_path)
@@ -86,6 +88,57 @@ class Vectorizer:
         # first column
         return self.model.layers.layers[0].layers[self.layer].outputs.asnumpyarray()[:, 0]
 
+    def get_32_attribute_vectors(self, img_path_array):
+        """
+        Given you are running on a GPU, you batch process 32 images at a time.
+
+        :param img_path_array: An array of 32 images
+        :return: A dict of the paths and their respective attribute vectors
+        """
+        # Ensure that a cpu user is not accessing a GPU command
+        if self.backend is not 'gpu':
+            raise GpuNotSupportedException(self.backend)
+
+        # Ensure all paths pass
+        faulty_paths = []
+        for img_path in img_path_array:
+            if not image_is_local(img_path):
+                faulty_paths.append(img_path)
+
+        if faulty_paths is not []:
+                raise ImageNotFound(faulty_paths)
+
+        # make an image buffer on host, pad out to batch size
+        host_buf = np.zeros((3 * self.patch_height * self.patch_width, self.model.be.bsz))
+
+        for img_index in range(len(img_path_array)):
+            im = imread(img_path_array[img_index]).astype(float)
+
+            # Fix the image into a flat array organised as [RRRRR..GGGGG..BBBB]
+            patch_array = self.patch_image(im)
+
+            # set the first image to be the image data loaded above
+            host_buf[:, img_index] = patch_array.copy()
+
+        # make buffer on the device
+        dev_buf = self.model.be.zeros((3 * self.patch_height * self.patch_width, self.model.be.bsz))
+        # copy host buffer to device buffer
+        dev_buf[:] = host_buf
+
+        # Send through the network.
+        predictions = self.model.fprop(dev_buf, True).asnumpyarray()[:, 0]
+        # print predictions
+
+        # Print the activations of the 4th layer from the end of the model
+        # Note 1: model.layers represents a SingleOutputTree when using GoogLeNet;
+        # during inference only the main branch (index 0) outputs are considered
+        img_vector_dict = []
+        for img_index in range(len(img_path_array)):
+            img_path = img_path_array[img_index]
+            img_vect = self.model.layers.layers[0].layers[self.layer].outputs.asnumpyarray()[:, img_index]
+            img_vector_dict[img_path] = img_vect
+        return img_vector_dict
+
     # Expects 256x256
     def patch_image(self, im):
         # convert to BGR
@@ -114,3 +167,24 @@ class Vectorizer:
         patch_array = patch.transpose((2, 0, 1)).flatten()
 
         return patch_array
+
+
+class GpuNotSupportedException(Exception):
+    def __init__(self, backend):
+        super(GpuNotSupportedException, self).__init__(
+           "GpuNotSupportedException: The vectorizer has the backend '{}' not 'gpu'".format(backend))
+
+
+class ImageNotFound(Exception):
+    paths = []
+
+    def __init__(self, paths):
+        if paths is None or paths == []:
+            raise AttributeError("paths is empty")
+        self.paths = paths
+
+        str_paths = ""
+        for path in paths:
+            str_paths += str(path)+os.linesep
+        super(ImageNotFound, self).__init__(
+            "File(s) Not Found: The following images do not exist{}{}".format(os.linesep, str_paths))
