f33cdf6511779a3df77f41d32ea98b38f037298f
Stefan Collier <sc22g13@soton.ac.uk>
2016-11-17 13:09:30 +0000

    Added print rather than csv save

--- service/olivia/test__gpu_vectorizer.py
+++ service/olivia/test__gpu_vectorizer.py
@@ -19,17 +19,11 @@ class VectorizerTest(unittest.TestCase):
     def test_32_image_processing(self):
         print 'About to vectorize'
         res_dict = self.vec.get_32_attribute_vectors(self.imagenames)
-        print 'done vectorizing'
-
-        data = deepcopy(res_dict.values())
-        # Save it locally
-        with open('results32.csv', 'wb') as f:
-            wtr = csv.writer(f, delimiter=',')
-            wtr.writerows(data)
-
-
-
-        self.assertEqual(0, 0)
+        print 'done vectorizing, results'
+        print '------'
+        for vec in res_dict.values():
+            print vec
+        print '------'
 
 
 if __name__ == '__main__':
