afca0e9167624a96b02e1d670620d6ee120d241d
Stefan Collier <sc22g13@soton.ac.uk>
2016-12-14 19:08:08 +0000

    outlined app usage of /download

---
+++ service/AppDownloadReactions.py
@@ -0,0 +1,7 @@
+def download_get():
+    return "<h1>Incorrect Usage</h1>\
+        <br>/download only accepts POST\
+        <br>POST expects list of urls and optionally an id for each of them"
+
+def download_post(urls, ids):
+    pass
